c3e2786214b8570a6cb142bf3e5f663db3095a01
Björn Benouarets <bbenouarets@proton.me>
2024-05-22 13:28:26 +0200

    Update

--- doc2pdf.py
+++ doc2pdf.py
@@ -9,10 +9,12 @@ word = comtypes.client.CreateObject("Word.Application")
 for filename in os.listdir(path):
     if filename.endswith(".doc") or filename.endswith(".docx"):
         doc_path = os.path.join(path, filename)
-        print(f"Path: {doc_path}")
-        print(f"Converting {doc_path} to PDF...")
+        abs_doc_path = os.path.abspath(doc_path)  # Get absolute path
+        print(f"Path: {abs_doc_path}")
+        print(f"Converting {abs_doc_path} to PDF...")
         pdf_path = os.path.join(path, filename.replace(".doc", ".pdf").replace(".docx", ".pdf").replace(" ", "_"))
-        doc = word.Documents.Open(doc_path)
-        doc.SaveAs(pdf_path, FileFormat=17)
+        abs_pdf_path = os.path.abspath(pdf_path)  # Get absolute path
+        doc = word.Documents.Open(abs_doc_path)
+        doc.SaveAs(abs_pdf_path, FileFormat=17)
         doc.Close()
-        print(f"Converted {doc_path} to PDF.")
+        print(f"Converted {abs_doc_path} to PDF.")
