c18716a64e93d8249ef895607542ef59e47475dd
Björn Benouarets <bbenouarets@proton.me>
2024-05-22 13:18:37 +0200

    Update

--- doc2pdf.py
+++ doc2pdf.py
@@ -6,9 +6,11 @@ path = "docs"
 for filename in os.listdir(path):
     if filename.endswith(".doc") or filename.endswith(".docx"):
         doc_path = os.path.join(path, filename)
-        print(f"Path: {doc_path}")
-        print(f"Converting {doc_path} to PDF...")
-        convert(doc_path)
-        print(f"Converted {doc_path} to PDF.")
+        # Remove spaces from the filename
+        new_doc_path = doc_path.replace(" ", "_")
+        # Create empty file with the new name
+        open(new_doc_path, "w").close()
+        # Rename the file
+        convert(doc_path, new_doc_path)
 
 print("All .doc and .docx files in the 'docs' folder have been converted to PDF.")
